8373e0706b9466aa258e9886b1b56f57fd3799c0
Niels Ganser <niels@herimedia.com>
2018-06-15 18:14:26 +0200

    Explicitly test non-ASCII account names

--- tests/test_ec.py
+++ tests/test_ec.py
@@ -70,6 +70,23 @@ class ECImporterTestCase(TestCase):
         with open(self.filename) as fd:
             self.assertTrue(importer.identify(fd))
 
+    def test_identify_with_exotic_account_name(self):
+        importer = ECImporter(self.iban, 'Assets:DKB:EC')
+
+        with open(self.filename, 'wb') as fd:
+            fd.write(_format('''
+                "Kontonummer:";"{iban} / Girökóntô, Γιροκοντώ, 預金, حساب البنك";
+
+                "Von:";"01.01.2018";
+                "Bis:";"31.01.2018";
+                "Kontostand vom 31.01.2017:";"5.000,01 EUR";
+
+                {header};
+            ''', dict(iban=self.iban, header=HEADER)))
+
+        with open(self.filename) as fd:
+            self.assertTrue(importer.identify(fd))
+
     def test_identify_with_formatted_iban(self):
         importer = ECImporter(self.formatted_iban, 'Assets:DKB:EC')
 
